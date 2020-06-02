a91d145ad74f48e8512ff834dc1d814f1af96427
Nhat Dinh <nguyendinhnat206@gmail.com>
2020-06-02 12:39:05 -0400

    Add some comments and output in architect template

--- src/main/java/com/test/TemplatesController.java
+++ src/main/java/com/test/TemplatesController.java
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TemplatesController {
 
+    // This is for gourmet template
     @GetMapping("/gourmet_catering")
     public String gourmet(){
         System.out.println("In gourmet catering template");
         return "templates_gourmet_catering";
     }
 
+    // This is for architect template
     @GetMapping("/templates_architect")
     public String architect(){
         System.out.println("In architect template");
+        System.out.println("Architect templet is from W3 school website.");
         return "templates_architect";
     }
 }
